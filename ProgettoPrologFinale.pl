e444df3864db66f36b86f23957f635c57e20cf96
santiagopdz <santiago.pedranzini@gmail.com>
2025-03-06 18:27:10 +0100

    Update ProgettoPrologFinale.pl

--- ProgettoPrologFinale.pl
+++ ProgettoPrologFinale.pl
@@ -1,5 +1,6 @@
 %%%Riccardo Piatti 909687, Santiago Pedranzini 884850
 %%% -*- Mode: Prolog -*-
+%%% huffman-codes.pl
 
 %%% hucodec_decode/3 true se Message è la decodifica di Bits secondo HuffmanTree
 
